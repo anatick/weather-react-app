2e8cc60019c224bbc90d21df2e65139ce96a06f6
anatick <91561543+anatick@users.noreply.github.com>
2022-04-11 11:19:39 +0100

    Update Weather.js

--- src/Weather.js
+++ src/Weather.js
@@ -23,7 +23,7 @@ export default function Weather(props){
 
   function search(){
     const apiKey = "15b20f7cbd51930154860d6bf63663ce";
-    let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`; 
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`; 
     axios.get(apiUrl).then(handleResponse);
 
   }
